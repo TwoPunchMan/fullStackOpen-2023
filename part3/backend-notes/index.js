a5eddcf84233862f5deb4a5c119471d11f71d516
twopunchman <ray.wang.pro@gmail.com>
2023-10-02 16:56:22 -0400

    Part 3 Note app added put method

--- part3/backend-notes/index.js
+++ part3/backend-notes/index.js
@@ -21,10 +21,10 @@ const unknownEndpoint = (request, response) => {
   })
 }
 
-app.use(express.json())
 app.use(cors())
-app.use(requestLogger)
 app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (req, res) =>{
   res.send('<h1>Hello world</h1>')
@@ -36,12 +36,17 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   Note
     .findById(req.params.id)
     .then(note => {
-      res.json(note)
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -63,13 +68,43 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-  res.status(204).end()
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note
+    .findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(res => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
