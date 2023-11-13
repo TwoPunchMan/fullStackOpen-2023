1bbc1eb63b8ee11df1e67cff012a0709d0f0b020
twopunchman <ray.wang.pro@gmail.com>
2023-11-13 00:29:58 -0500

    Exercise 5.1 done added frontend login form/handling

--- part5/blog-list/frontend/src/App.jsx
+++ part5/blog-list/frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react'
 
 import Blog from './components/Blog'
+
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService
@@ -14,9 +19,55 @@ const App = () => {
       )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+      setUserName('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin} >
+        <h2>log in to application</h2>
+        <div>
+          username <input type="text" value={username} name="Username" onChange={({ target }) => setUserName(target.value)} />
+        </div>
+        <div>
+          password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    )
+  }
+
+  if (user === null) {
+    return loginForm()
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+
+      <div>{user.name} logged in</div>
+      <br></br>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
