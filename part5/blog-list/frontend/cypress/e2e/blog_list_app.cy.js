ae2dc19ceba680ee757144bf188e18bc00c8dde0
twopunchman <ray.wang.pro@gmail.com>
2024-01-14 01:52:43 -0500

    Exercise 5.17 Added cypress testing/wrote test showing login form; cypress.config.js frontend; testing.js backend

---
+++ part5/blog-list/frontend/cypress/e2e/blog_list_app.cy.js
@@ -0,0 +1,16 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      username: 'theYesMan',
+      password: 'nukethencr',
+      name: 'Marty Robbins'
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.visit('')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('log in to application')
+  })
+})
