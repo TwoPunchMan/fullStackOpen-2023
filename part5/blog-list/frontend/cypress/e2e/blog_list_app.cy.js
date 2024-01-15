b4cee87b7655a620afa79caf55860fb8de103e6e
twopunchman <ray.wang.pro@gmail.com>
2024-01-15 01:37:37 -0500

    Exercise 5.18 added end2end testing for login with right/wrong credentials

--- part5/blog-list/frontend/cypress/e2e/blog_list_app.cy.js
+++ part5/blog-list/frontend/cypress/e2e/blog_list_app.cy.js
@@ -13,4 +13,26 @@ describe('Blog app', function() {
   it('Login form is shown', function() {
     cy.contains('log in to application')
   })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('log in to application')
+      cy.get('#username').type('theYesMan')
+      cy.get('#password').type('nukethencr')
+      cy.get('#login-btn').click()
+
+      cy.contains('Marty Robbins logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in to application')
+      cy.get('#username').type('thiswillfail')
+      cy.get('#password').type('notpassword')
+      cy.get('#login-btn').click()
+
+      cy.get('.notification').should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+    })
+  })
 })
--- part5/blog-list/frontend/src/components/LoginForm.jsx
+++ part5/blog-list/frontend/src/components/LoginForm.jsx
@@ -15,6 +15,7 @@ const LoginForm = ({ login }) => {
       <div>
         username
         <input
+          id="username"
           type="text"
           value={username}
           name="Username"
@@ -24,13 +25,14 @@ const LoginForm = ({ login }) => {
       <div>
         password
         <input
+          id="password"
           type="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-btn" type="submit">login</button>
     </form>
   )
 }
