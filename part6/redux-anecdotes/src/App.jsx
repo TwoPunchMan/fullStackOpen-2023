ad642f8c0740a9a00845e0c10e21a1df44825fb1
twopunchman <ray.wang.pro@gmail.com>
2024-03-20 22:16:09 -0400

    Redux-anecdotes can now be tracked by git

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from './reducers/anecdoteReducer'
+
+import AnecdoteForm from './components/AnecdoteForm'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id))
+  }
+
+  const byVotes = (a, b) => b.votes - a.votes
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.sort(byVotes).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>Create new</h2>
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
