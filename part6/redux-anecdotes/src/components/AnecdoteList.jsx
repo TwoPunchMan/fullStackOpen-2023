247687c4ef8c1feb34cf5fe240f0aef5c035842a
twopunchman <ray.wang.pro@gmail.com>
2024-03-20 22:36:04 -0400

    Exercise 6.7-6.8 refactored App.jsx into AnecdoteForm/AnecdoteList components and reducer

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,41 @@
-const AnecdoteList = () => {
+import { useDispatch, useSelector } from "react-redux"
+import { voteAnecdote } from "../reducers/anecdoteReducer"
 
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div key={anecdote.id}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id))
+  }
+
+  const byVotes = (a, b) => b.votes - a.votes
+
+  return (
+    <div>
+      {anecdotes.sort(byVotes).map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => vote(anecdote.id)}
+        />
+      )}
+    </div>
+  )
 }
 
 export default AnecdoteList
