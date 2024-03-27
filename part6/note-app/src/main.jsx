f8a88b17cab54ece3fdca2955cacb54880315bcf
twopunchman <ray.wang.pro@gmail.com>
2024-03-27 08:28:49 -0400

    Added @reduxjs/toolkit and refactored reducers with slice function/refactored test file

--- part6/note-app/src/main.jsx
+++ part6/note-app/src/main.jsx
@@ -1,49 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
-
-console.log(store.getState())
-
-/*
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
   }
 })
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
 )
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
