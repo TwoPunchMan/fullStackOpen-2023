e594b4278fa132fc650779016fe0e781f1d3f3e4
twopunchman <ray.wang.pro@gmail.com>
2024-03-29 23:57:00 -0400

    Abstracted communication with backend separate from components

--- part6/note-app/src/App.jsx
+++ part6/note-app/src/App.jsx
@@ -1,24 +1,27 @@
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 
-import noteService from './services/notes'
-import { setNotes } from './reducers/noteReducer'
-import { useDispatch } from 'react-redux'
+import { initializeNotes } from './reducers/noteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
+    dispatch(initializeNotes())
+  }, [])
+
+/*  useEffect(() => {
     noteService
       .getAll()
       .then(notes =>
         dispatch(setNotes(notes))
       )
   }, [])
-
+*/
 
   return (
     <div>
