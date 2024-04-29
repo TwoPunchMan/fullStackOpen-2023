c6a20c7f32141d24ac97fb3a48c9ba15b258cf26
twopunchman <ray.wang.pro@gmail.com>
2024-04-29 16:10:17 -0400

    Exercises 7.1-7.2 added Router and Link to Menu component and added Routing for individual Anecdote(s)

--- part7/navbar/src/App.jsx
+++ part7/navbar/src/App.jsx
@@ -1,63 +1,147 @@
 import { useState } from 'react'
-import ReactDOM from 'react-dom/client'
+import {
+  Routes, Route, Link,
+  Navigate,
+  useParams, useNavigate,
+  useMatch
+} from 'react-router-dom'
 
-const Home = () => {
+const Home = () => (
   <div>
-    <h2>TKTL Notes App</h2>
+    <h2>TKTL notes app</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa qui officia deserunt mollit anim id est laborum.
+    </p>
   </div>
+)
+
+const Note = ({ note }) => {
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'important' : ''}</strong></div>
+    </div>
+  )
 }
 
-const Notes = () => {
+const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
+    </ul>
   </div>
-}
+)
 
-const Users = () => {
+const Users = () => (
   <div>
     <h2>Users</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
   </div>
-}
+)
 
+const Login = (props) => {
+  const navigate =  useNavigate()
 
-
-const App = () => {
-  const [page, setPage] = useState('home')
-
-  const toPage = (page) => (event) => {
+  const onSubmit = (event) => {
     event.preventDefault()
-    setPage(page)
+    props.onLogin('mluukkai')
+    navigate('/')
   }
 
-  const content = () => {
-    if (page === 'home') {
-      return <Home />
-    } else if (page === 'notes') {
-      return <Notes />
-    } else if (page === 'users') {
-      return <Users />
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type='password' />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
     }
+  ])
+
+  const [user, setUser] = useState(null)
+
+  const login = (user) => {
+    setUser(user)
   }
 
   const padding = {
     padding: 5
   }
 
+  const match = useMatch('/notes/:id')
+  const note = match
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
+
   return (
     <div>
       <div>
-        <a href="" onClick={toPage('home')} style={padding}>
-          home
-        </a>
-        <a href="" onClick={toPage('notes')} style={padding}>
-          notes
-        </a>
-        <a href="" onClick={toPage('users')} style={padding}>
-          users
-        </a>
+        <div>
+          <Link style={padding} to="/">home</Link>
+          <Link style={padding} to="/notes">notes</Link>
+          <Link style={padding} to="/users">users</Link>
+          {user
+            ? <em>{user} logged in</em>
+            : <Link style={padding} to="/login">login</Link>
+          }
+        </div>
+
+        <Routes>
+          <Route path="/notes/:id" element={<Note note={note} />} />
+          <Route path="/notes" element={<Notes notes={notes} />} />
+          <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+          <Route path="/login" element={<Login onLogin={login} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
 
-      {content()}
+      <footer>
+        <br />
+        <em>Note app, Department of Computer Science 2024</em>
+      </footer>
     </div>
   )
 }
